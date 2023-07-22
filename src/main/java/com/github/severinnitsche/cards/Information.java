ece3636af25804c65c97e427276968d1aa0651d4
Severin-Nitsche <severinnitsche@gmail.com>
2023-07-22 19:57:18 +0200

    feat: Update the turn information

--- src/main/java/com/github/severinnitsche/cards/Information.java
+++ src/main/java/com/github/severinnitsche/cards/Information.java
@@ -2,10 +2,12 @@ package com.github.severinnitsche.cards;
 
 /**
  * The information a player receives at every turn
+ * @param drawCards the number of cards to draw or 0
+ * @param wish the color to play or null
  * @param stack the stack of cards played
  * @param hand the player's hand
  * @param numberOfCards every player's number of cards
  * @param playerNumber the index of the current player
  */
-public record Information(Card[] stack, Hand hand, int[] numberOfCards, int playerNumber) {
+public record Information(int drawCards, Color wish, Card[] stack, Hand hand, int[] numberOfCards, int playerNumber) {
 }
